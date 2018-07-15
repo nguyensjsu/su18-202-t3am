7ddfa2ae671ce3773b7644153284a5c4708e56b8
Sy Le <lenguyensy@gmail.com>
2018-07-15 11:38:22 -0700

    added some classes for user profile

---
+++ starbucks2-domain/src/java/dao/UserProfileDao.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.List;
+
+/**
+ *
+ * @author syle
+ */
+public class UserProfileDao implements BaseDao{
+
+    @Override
+    public Object create(Object o) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object update(Object o) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object find(String key) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List list(String kw) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
