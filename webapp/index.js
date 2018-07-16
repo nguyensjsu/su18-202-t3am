c2c29a5e776e780eb36f2ffde9712f6e6ce1ac22
Sy Le <lenguyensy@gmail.com>
2018-07-15 19:24:20 -0700

    make good progress with load cards

--- webapp/index.js
+++ webapp/index.js
@@ -4,8 +4,10 @@ localStorage['my_user_id'] = '2c60158e-d432-4b78-a300-360cc6fa7260';
 (function(){
   var API_URL_GET_CARDS = 'http://localhost:8202/api/v1/cards';
   var API_URL_GET_USER_PROFILE = 'http://localhost:8202/api/v1/user_profile';
+  var API_URL_GET_PURCHASES = 'http://localhost:8202/api/v1/purchases';
   var API_URL_ = 'http://localhost:8202/api/v1/cards';
-  var API_URL_ = 'http://localhost:8202/api/v1/cards';
+
+  var formatter_date_full = 'MMMM Do YYYY, h:mm:ss a';
 
   var data = {
     user: null,
@@ -42,6 +44,8 @@ localStorage['my_user_id'] = '2c60158e-d432-4b78-a300-360cc6fa7260';
 
     $('#page-login').hide();
     $('#page-authenticated').show();
+
+    window.refresh();
   }
 
   function _onLoggedOff() {
@@ -65,17 +69,19 @@ localStorage['my_user_id'] = '2c60158e-d432-4b78-a300-360cc6fa7260';
 
   // state
   function _isLoggedIn(){
-    return !!data.user;
+    if(!localStorage['my_secure_token'] || !localStorage['my_user_id']){
+      return false;
+    }
+
+    return true;
   }
 
 
   // methods
   function _doLogIn(){
-    if(!localStorage['my_secure_token'] || !localStorage['my_user_id']){
+    if(!_isLoggedIn()){
       return Promise.reject();
     }
-
-
 
     return new Promise((resolve, reject) => {
       fetch(
@@ -111,10 +117,102 @@ localStorage['my_user_id'] = '2c60158e-d432-4b78-a300-360cc6fa7260';
 
 
   function _loadCards() {
+    var promiseAjax;
+    var listEl = '#card-list';
+    var url = `${API_URL_GET_CARDS}?uid=${localStorage['my_user_id']}`;
+    if(!_isLoggedIn()){
+      promiseAjax = Promise.reject();
+    } else {
+      promiseAjax = new Promise((resolve, reject) => {
+        fetch(
+          url,
+          {
+            headers:{
+              'Content-Type': 'application/json'
+            }
+          }
+        )
+          .then(res => res.json())
+          .catch(reject)
+          .then(resolve);
+      });
+    }
 
+    $(listEl).empty();
+
+    promiseAjax.then(function(resp){
+      if(resp.length === 0){
+        throw '';
+      }
+
+      // [{"number":"123456789","code":"321","balance":20.0,"date_added":1524958728440,"uid":"2c60158e-d432-4b78-a300-360cc6fa7260"}]
+
+      resp.forEach(function(item){
+        $(`
+          <tr>
+            <td>${item.number}</td>
+            <td>${item.code}</td>
+            <td>${item.balance}</td>
+            <td>${moment(item.date_added).format(formatter_date_full)}</td>
+          </tr>
+          `).appendTo(listEl)
+      })
+    }).catch(() => {
+      $(`
+        <tr>
+          <td colspan="4">No Result</td>
+        </tr>
+        `).appendTo(listEl)
+    });
   }
 
   function _loadPurchase() {
+    var promiseAjax;
+    var listEl = '#purchase-list';
+    var url = `${API_URL_GET_PURCHASES}?uid=${localStorage['my_user_id']}`;
+    if(!_isLoggedIn()){
+      promiseAjax = Promise.reject();
+    } else {
+      promiseAjax = new Promise((resolve, reject) => {
+        fetch(
+          url,
+          {
+            headers:{
+              'Content-Type': 'application/json'
+            }
+          }
+        )
+          .then(res => res.json())
+          .catch(reject)
+          .then(resolve);
+      });
+    }
 
+    $(listEl).empty();
+
+    promiseAjax.then(function(resp){
+      if(resp.length === 0){
+        throw '';
+      }
+
+      // [{"number":"123456789","code":"321","balance":20.0,"date_added":1524958728440,"uid":"2c60158e-d432-4b78-a300-360cc6fa7260"}]
+
+      resp.forEach(function(item){
+        $(`
+          <tr>
+            <td>${item.number}</td>
+            <td>${item.code}</td>
+            <td>${item.balance}</td>
+            <td>${moment(item.date_added).format(formatter_date_full)}</td>
+          </tr>
+          `).appendTo(listEl)
+      })
+    }).catch(() => {
+      $(`
+        <tr>
+          <td colspan="4">No Result</td>
+        </tr>
+        `).appendTo(listEl)
+    });
   }
 })()
