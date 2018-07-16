17d68efed16a8d63d32ee098fc5f46eee47535fb
h7shin <hyunwook.shin@sjsu.edu>
2018-07-16 07:57:57 +0000

    Unit test for fromJsonForObjList()

--- starbucks2-common/src/test/java/JSONHelperTest.java
+++ starbucks2-common/src/test/java/JSONHelperTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.List;
 
 import helper.JSONHelper;
 
@@ -32,6 +33,18 @@ public class JSONHelperTest {
        
        assertEquals(321, d.getA());
        assertEquals("BB", d.getB());
+    }
+
+    @Test
+    public void FromJsonForObjList() throws Exception {
+       // todo expand
+       String json = "[{ \"a\": 321, \"b\" : \"BB\"}]";
+       List<DummyClass> lst = JSONHelper.fromJsonForObjList( json, DummyClass.class );
+       
+      for (DummyClass d : lst) {
+          assertEquals(321, d.getA());
+          assertEquals("BB", d.getB());
+       }
     }
 
     @After
