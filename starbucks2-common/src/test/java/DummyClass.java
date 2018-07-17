d789f5c777803f7b260bca7d8fd313f79d24cd17
h7shin <hyunwook.shin@sjsu.edu>
2018-07-17 01:24:28 +0000

    Add DummyClass.java

---
+++ starbucks2-common/src/test/java/DummyClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+/**
+ *
+ * @author Hyunwook Shin
+ */
+public class DummyClass {
+    int a;
+    String b;
+
+    public int  getA(){
+       return a;
+    }
+
+    public String getB(){
+       return b;
+    }
+}
