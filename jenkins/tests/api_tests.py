cc87c0a06270ef5e3c625ac83737b48823fde057
h7shin <hyunwook.shin@sjsu.edu>
2018-07-20 21:39:11 +0000

    Add signing api test

--- jenkins/tests/api_tests.py
+++ jenkins/tests/api_tests.py
@@ -12,6 +12,12 @@ TEST_UID = '958bbc20-cda2-4f57-9fe6-e1ecb6e6e913'
 def checkResponse( response ):
    return response.ok
 
+# While we use the unittest library, it is
+# applied for testing end-to-end system.
+# The test expects the backend server to
+# be running in localhost listening to
+# port 8202
+
 class TestAPIs( unittest.TestCase ):
 
    def testGetCards( self ):
@@ -25,6 +31,17 @@ class TestAPIs( unittest.TestCase ):
          assert len( entry[ 'number' ] ) == 9
       assert checkResponse( response )
 
+   def testSignIn( self ):
+      testemail =  "syle1@gmail.com"
+      endpoint = "/api/v1/signin"
+      url = URL.format( endpoint=endpoint, query="" )
+      data = '{"email": ' + testemail + ', "password" :"password"}'
+      response = requests.post( url, data=data )
+      info = json.loads( response.text )
+      assert 'user_id' in info
+      assert 'balance' in info
+      assert testemail == info[ 'email' ]
+  
 
 if __name__ == "__main__":
    unittest.main(argv=sys.argv[1:])
